e047d1c4abad34374811d1f879ec0a231072b7ce
MahamudulAmin <mahamudulamin100@gmail.com>
2026-07-23 12:17:12 +0600

    login page and update student dashboard and pages

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,59 +1,31 @@
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // clear user data if you store any
+    localStorage.removeItem("user");
+
+    // go to login page
+    navigate("/login");
+  };
+
+
   return (
-    <nav
-      style={{
-        height: "70px",
-        backgroundColor: "#1E3A8A",
-        color: "white",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "0 30px",
-        boxShadow: "0 2px 8px rgba(0,0,0,0.2)",
-      }}
-    >
-      <div>
-        <h2 style={{ margin: 0 }}>Campus Management System</h2>
-      </div>
+    <nav className="navbar">
 
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "20px",
-        }}
-      >
-        <span style={{ cursor: "pointer", fontSize: "22px" }}>🔔</span>
+      <h2>
+        Campus Management System
+      </h2>
 
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "10px",
-          }}
-        >
-          <div
-            style={{
-              width: "40px",
-              height: "40px",
-              borderRadius: "50%",
-              backgroundColor: "#fff",
-              color: "#1E3A8A",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              fontWeight: "bold",
-            }}
-          >
-            M
-          </div>
 
-          <div>
-            <h4 style={{ margin: 0 }}>Mahamudul Amin</h4>
-            <small>Student</small>
-          </div>
-        </div>
-      </div>
+      <button onClick={handleLogout}>
+        Logout
+      </button>
+
+
     </nav>
   );
 };
