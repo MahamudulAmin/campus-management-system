f5b3451e82e2183c56209ebc0c2e9b41cfbdcc7a
MahamudulAmin <mahamudulamin100@gmail.com>
2026-08-08 11:40:42 +0600

    change import

--- Frontend/src/pages/student/StudentDashboard.tsx
+++ Frontend/src/pages/student/StudentDashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import Navbar from "../../components/Navbar";
+// import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar";
 import SummaryCard from "../../components/SummaryCard";
 import OfficeCard from "../../components/OfficeCard";
